01990e960da9370a6eac14c47c6d2e06ed0e2221
Timofey Chebaturkin <117652910+Het76@users.noreply.github.com>
2024-11-02 16:57:29 +0300

    Update blumforbrowser.user.js

--- blumforbrowser.user.js
+++ blumforbrowser.user.js
@@ -2,13 +2,13 @@
 // @name         Blum Autoclicker
 // @version      2.5
 // @namespace    Violentmonkey Scripts
-// @author       mudachyo
+// @author       het
 // @match        https://telegram.blum.codes/*
 // @grant        none
 // @icon         https://cdn.prod.website-files.com/65b6a1a4a0e2af577bccce96/65ba99c1616e21b24009b86c_blum-256.png
-// @downloadURL  https://github.com/mudachyo/Blum/raw/main/blum-autoclicker.user.js
-// @updateURL    https://github.com/mudachyo/Blum/raw/main/blum-autoclicker.user.js
-// @homepage     https://github.com/mudachyo/Blum
+// @downloadURL  https://github.com/Het76/blum/edit/main/blumforbrowser.user.js
+// @updateURL    https://github.com/Het76/blum/edit/main/blumforbrowser.user.js
+// @homepage     https://github.com/Het76/blum
 // ==/UserScript==
 
 let GAME_SETTINGS = {
